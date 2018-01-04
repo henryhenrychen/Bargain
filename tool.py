a27c27e331fb61cf174c7468efcdc1269584a2b4
henryhenrychen <b02902057@ntu.edu.tw>
2018-01-04 16:17:50 +0800

    log parser

---
+++ tool.py
@@ -0,0 +1,49 @@
+import json
+import os
+from IPython import embed
+import re
+def clean_phrase(line):
+    return line
+
+def parse_log(file_name):
+    data = json.load(open(file_name))
+    EOS = ' <eos> '
+    ans = ''
+    input_ = '<input> '+' '.join([str(i) for unit in data['input'] for i in unit]) \
+        + ' </input> '
+    dialogue = '<dialogue> '
+    for unit in data['dialogue']:
+        m = re.search('<choose (.+?)>', unit['msg'])
+        if m :
+            fruit = m.group(1)
+            buyer = unit['agent']
+            break
+    #too see who's first
+    switch = ['BUYER: ', 'SELLER: ']
+    if buyer == data['dialogue'][0]['agent'] :
+        ptr = 0
+    else :
+        ptr = 1
+
+    for unit in data['dialogue'] :
+        dialogue += switch[ptr]+unit['msg']+ EOS
+        ptr = (ptr+1)%2
+    dialogue += ' </dialogue> '
+    output = ' <output> '+ fruit + ' </outpupt> '
+    rating = ' <rating> '+ str(data['rating'])+ ' </rating>\n '
+
+    return(input_+dialogue+output+rating)
+
+def extend_train_data(train_file, log_path):
+    if os.path.isdir(log_path) :
+        if not log_path[-1]=='/':
+            log_file += '/'
+        with open(train_file, 'w') as F:
+            names = [f for f in os.listdir(log_path) if f.endswith('.txt')]
+            for name in names :
+                F.write(parse_log(log_path+name))
+
+    else :
+        with open(train_file, 'a+') as f:
+            f.write(parse_log(log_path))
+
