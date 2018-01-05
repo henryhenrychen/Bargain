673650e992f37863b71bd2889cfcff4b2ed4d60e
henryhenrychen <b02902057@ntu.edu.tw>
2018-01-05 10:59:10 +0800

    preprocessing

--- tool.py
+++ tool.py
@@ -5,20 +5,22 @@ import re
 import nltk
 
 def clean_phrase(line):
-    #return nltk.word_tokenize(line.lower())
-    return line
+    return ' '.join(nltk.word_tokenize(line.lower()))
+    #return line
 def parse_log(file_name):
     data = json.load(open(file_name))
     EOS = ' <eos> '
+    CHO = ' <choose> '
     ans = ''
     input_ = '<input> '+' '.join([str(i) for unit in data['input'] for i in unit]) \
         + ' </input> '
     dialogue = '<dialogue> '
-    for unit in data['dialogue']:
-        m = re.search('<choose (.+?)>', unit['msg'])
+    for idx, unit in enumerate(data['dialogue']):
+        m = re.search('<choose ([a-z]+)>', unit['msg'])
         if m :
             fruit = m.group(1)
             buyer = unit['agent']
+            #data['dialogue'][idx]['msg'] = '<choose>'
             break
     #too see who's first
     switch = ['BUYER: ', 'SELLER: ']
@@ -28,7 +30,11 @@ def parse_log(file_name):
         ptr = 1
 
     for unit in data['dialogue'] :
-        dialogue += switch[ptr]+unit['msg']+ EOS
+        m = re.search('<choose ([a-z]+)>', unit['msg'])
+        if m:
+            dialogue += switch[ptr]+ CHO + EOS
+        else :
+            dialogue += switch[ptr]+clean_phrase(unit['msg'])+ EOS
         ptr = (ptr+1)%2
     dialogue += ' </dialogue> '
     output = ' <output> '+ fruit + ' </outpupt> '
@@ -39,7 +45,7 @@ def parse_log(file_name):
 def extend_train_data(train_file, log_path):
     if os.path.isdir(log_path) :
         if not log_path[-1]=='/':
-            log_file += '/'
+            log_path += '/'
         with open(train_file, 'w') as F:
             names = [f for f in os.listdir(log_path) if f.endswith('.txt')]
             for name in names :
